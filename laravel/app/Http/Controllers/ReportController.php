3790885fe3609d6755d04ea670e5c463ba309bf2
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-03-20 17:09:07 -0500

    Agregando EndPoint para listar los reportes  Changes to be committed: 	modified:   app/Http/Controllers/ReportController.php 	modified:   routes/api.php

--- laravel/app/Http/Controllers/ReportController.php
+++ laravel/app/Http/Controllers/ReportController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Exports\UsersReport;
 use App\Models\Report;
-use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Carbon;
 use App\Http\Requests\ReportGenerateRequest;
 
@@ -21,14 +20,22 @@ class ReportController extends Controller
         $report->created_at  = Carbon::now();
         $report->report_link = $file_name;
         $report->save();
-
-        // Excel::store((new UsersReport($request->start_date,$request->end_date)), $file_name, 'public');
         
          (new UsersReport($request->start_date,$request->end_date))->store( $file_name, 'public');
         
         return response()->json([
             'status'=>'success',
             'message' => 'El reporte se esta generando.'
+        ],201);
+    }
+
+    public function listReports(Request $request)
+    {
+        $reports = Report::paginate(10);
+
+        return response()->json([
+            'status'=>'success',
+            'data' => $reports
         ],200);
     }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -22,3 +22,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/generate-report',[ReportController::class,'generateReport']);
+Route::get('/list-reports',[ReportController::class,'listReports']);
